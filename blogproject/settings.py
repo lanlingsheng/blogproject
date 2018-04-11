f3e36f7b36382320681be07b306d2c04367860c1
lanlingsheng <781192559@qq.com>
2018-04-11 22:23:03 +0800

    改变 CDN

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    os.path.join(BASE_DIR), '/home/gaoyan/sites/blogproject/blog/static',
 ]
 
 # haystack settings
