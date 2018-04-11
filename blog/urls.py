ae9830c394f9f6bd50d6ad1f2445be479a946d93
lanlingsheng <781192559@qq.com>
2018-04-11 21:53:47 +0800

    改变 CDN

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -11,4 +13,4 @@ urlpatterns = [
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
     url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
     url(r'^tag/(?P<pk>[0-9]+)/$', views.TagsView.as_view(), name='tag'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
